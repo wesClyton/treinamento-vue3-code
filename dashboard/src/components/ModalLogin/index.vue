2a56705c3d635721c94214f3b638f2a98343449a
DESKTOP-OVEN28P\Weslley-PC <wes.hinsch@gmail.com>
2021-02-16 12:16:47 -0300

    Incluisão da biblioteca Toast Finalização do modal login Finalização do processo de login

--- dashboard/src/components/ModalLogin/index.vue
+++ dashboard/src/components/ModalLogin/index.vue
@@ -12,28 +12,28 @@
           v-model="state.email.value"
           type="email"
           :class="{
-            'border-brand-danger': !state.email.errorMessage
+            'border-brand-danger': !!state.email.errorMessage
           }"
-          class="block w-full -px-4 py-3 mt-1 text-lg bg-gray-100 bordr-2 rounded"
+          class="block w-full -px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="jane.doe@gmail.com">
         <span
-          v-if="!state.email.errorMessage"
+          v-if="!!state.email.errorMessage"
           class="block font-medium texte-brand-danger"
         >{{state.email.errorMessage}}</span>
       </label>
 
       <label class="block">
-        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <span class="text-lg mt-9 font-medium text-gray-800">Senha</span>
         <input
           v-model="state.password.value"
           type="password"
           :class="{
-            'border-brand-danger': !state.password.errorMessage
+            'border-brand-danger': !!state.password.errorMessage
           }"
-          class="block w-full -px-4 py-3 mt-1 text-lg bg-gray-100 bordr-2 rounded"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
           placeholder="jane.doe@gmail.com">
         <span
-          v-if="!state.password.errorMessage"
+          v-if="!!state.password.errorMessage"
           class="block font-medium texte-brand-danger"
         >{{state.password.errorMessage}}</span>
       </label>
@@ -44,7 +44,10 @@
         :class="{
           'opacity-50': state.isLoading
         }"
-        class="px-8 py-3 mt-10 texte-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150">Entrar</button>
+        class="px-8 py-3 mt-10 texte-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150">
+          <icon v-if="state.isLoading" name="loading" class="animate-spin" />
+          <span v-else>Entrar</span>
+      </button>
 
     </form>
   </div>
@@ -54,26 +57,81 @@
 <script>
 
 import { reactive } from 'vue'
+import { useField } from 'vee-validate'
 import useModal from '../../hooks/useModal'
+import Icon from '../icon'
+import { validateEmptyAndLegth3, validateEmptyAndEmail } from '../../utils/validators'
+import services from '../../services'
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 export default {
+  components: {
+    Icon
+  },
   setup () {
     const modal = useModal()
+    const router = useRouter()
+    const toast = useToast()
+
+    const {
+      value: emailValue,
+      errorMessage: emailErrorMessage
+    } = useField('email', validateEmptyAndEmail)
+
+    const {
+      value: passwordValue,
+      errorMessage: passwordErrorMessage
+    } = useField('password', validateEmptyAndLegth3)
+
     const state = reactive({
       hasErrors: false,
       isLoading: false,
       email: {
-        value: '',
-        errorMessage: ''
+        value: emailValue,
+        errorMessage: emailErrorMessage
       },
       password: {
-        value: '',
-        errorMessage: ''
+        value: passwordValue,
+        errorMessage: passwordErrorMessage
       }
     })
 
-    function handleSubmit () {
-      console.log()
+    async function handleSubmit () {
+      try {
+        toast.clear()
+        state.isLoading = true
+        const { data, errors } = await services.auth.login({
+          email: state.email.value,
+          password: state.password.value
+        })
+
+        if (!errors) {
+          window.localStorage.setItem('token', data.token)
+          router.push({ name: 'Feedbacks' })
+          state.isLoading = false
+          modal.close()
+          return
+        }
+
+        if (errors.status === 404) {
+          toast.error('Email não encontrado')
+        }
+
+        if (errors.status === 401) {
+          toast.error('Email/Senha inválidos')
+        }
+
+        if (errors.status === 400) {
+          toast.error('Ocorreu um erro ao fazer o login')
+        }
+
+        state.isLoading = false
+      } catch (error) {
+        state.isLoading = false
+        state.hasErrors = !!error
+        toast.error('Ocorreu um erro ao fazer o login')
+      }
     }
 
     return {
---
+++ dashboard/src/views/Feedbacks/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>FeedBacks</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
